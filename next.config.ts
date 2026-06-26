4ef11abe14117cebd9588f368ba4fe8ded4b2312
Anthony Cueva <hi@cueva.io>
2026-06-26 01:23:16 -0500

    Add short link redirects

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,32 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/whatsapp",
+        destination:
+          "https://chat.whatsapp.com/JH4oq2pmeX43EWXkvRhQHC?mode=gi_t",
+        permanent: false,
+      },
+      {
+        source: "/wpp",
+        destination:
+          "https://chat.whatsapp.com/JH4oq2pmeX43EWXkvRhQHC?mode=gi_t",
+        permanent: false,
+      },
+      {
+        source: "/luma",
+        destination: "https://luma.com/hack0-zrbp",
+        permanent: false,
+      },
+      {
+        source: "/event",
+        destination: "https://luma.com/hack0-zrbp",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
